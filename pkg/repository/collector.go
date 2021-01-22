1c761f6ad25b18b76317b728c2276dcd730be3ef
Alexander Bergasov <a.bergasov@city-mobil.ru>
2021-01-22 21:33:14 +0300

    54815

--- pkg/repository/collector.go
+++ pkg/repository/collector.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -14,31 +15,45 @@ type event struct {
 	label string
 }
 
-const rangeMax int32 = 124
+const rangeMax int32 = 4
 
 type Collector struct {
-	collector chan *event
-	dataMx    sync.Mutex
-	data      map[string]int
-	rangeMax  int32
-	db        *storage.DBConnector
+	dataMxContainer    []*sync.Mutex
+	dataContainer      []map[string]int
+	collectorContainer []chan *event
+	reqCounter         int32
+	db                 *storage.DBConnector
 }
 
 func NewCollector(db *storage.DBConnector) *Collector {
 	cl := &Collector{
-		collector: make(chan *event, 1000),
-		data:      make(map[string]int),
-		rangeMax:  rangeMax,
-		db:        db,
+		dataContainer:      make([]map[string]int, rangeMax, rangeMax),
+		collectorContainer: make([]chan *event, rangeMax, rangeMax),
+		dataMxContainer:    make([]*sync.Mutex, rangeMax, rangeMax),
+		db:                 db,
+		reqCounter:         0,
 	}
 
-	go cl.collectEvents()
-	go cl.saveEvents()
+	for i := 0; i < int(rangeMax); i++ {
+		cl.dataContainer[i] = make(map[string]int, 1000)
+		cl.collectorContainer[i] = make(chan *event, 1000)
+		cl.dataMxContainer[i] = &sync.Mutex{}
+		go cl.collectEvents(i)
+		go cl.saveEvents(i)
+	}
+
+	go func() {
+		for range time.Tick(1 * time.Second) {
+			atomic.StoreInt32(&cl.reqCounter, 0)
+		}
+	}()
+
 	return cl
 }
 
 func (cl *Collector) HandleEvent(id int32, label string) {
-	cl.collector <- &event{id: id, label: label}
+	i := atomic.AddInt32(&cl.reqCounter, 1) % rangeMax
+	cl.collectorContainer[i] <- &event{id: id, label: label}
 }
 
 func (cl *Collector) GetState() []EventStat {
@@ -50,29 +65,29 @@ func (cl *Collector) GetState() []EventStat {
 	return p
 }
 
-func (cl *Collector) collectEvents() {
-	for i := range cl.collector {
-		lbl := strconv.Itoa(int(i.id)) + "_" + i.label
-		cl.dataMx.Lock()
-		if _, ok := cl.data[lbl]; !ok {
-			cl.data[lbl] = 0
+func (cl *Collector) collectEvents(i int) {
+	for e := range cl.collectorContainer[i] {
+		lbl := strconv.Itoa(int(e.id)) + "_" + e.label
+		cl.dataMxContainer[i].Lock()
+		if _, ok := cl.dataContainer[i][lbl]; !ok {
+			cl.dataContainer[i][lbl] = 0
 		}
-		cl.data[lbl]++
-		cl.dataMx.Unlock()
+		cl.dataContainer[i][lbl]++
+		cl.dataMxContainer[i].Unlock()
 	}
 }
 
-func (cl *Collector) saveEvents() {
+func (cl *Collector) saveEvents(i int) {
 	for range time.Tick(5 * time.Second) {
-		cl.dataMx.Lock()
-		counterData := cl.data
-		cl.data = make(map[string]int)
-		cl.dataMx.Unlock()
+		cl.dataMxContainer[i].Lock()
+		counterData := cl.dataContainer[i]
+		cl.dataContainer[i] = make(map[string]int, len(counterData))
+		cl.dataMxContainer[i].Unlock()
 		values := make([]interface{}, 0, 30)
 		placeHolders := make([]string, 0, 10)
-		for i, v := range counterData {
+		for j, v := range counterData {
 			placeHolders = append(placeHolders, "(?,?,?)")
-			data := strings.Split(i, "_")
+			data := strings.Split(j, "_")
 			values = append(values, data[0], data[1], v)
 			if len(placeHolders) >= 10 {
 				cl.insertData(placeHolders, values)
